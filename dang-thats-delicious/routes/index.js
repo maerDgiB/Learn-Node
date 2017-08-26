1a0131b51169fc2c382b6eabee1326e39be5e5a9
Kevin <1041874421@qq.com>
2017-08-26 12:40:45 +0800

    [TODO] node@8.X,由于低版本安装过，导致node-sass出现问题，最后需要npm rebuild node-sass 一下

--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -3,7 +3,15 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  const baiji = {name:"baiji",age:21,"cool":true}
+  // res.send('Hey! It works!');
+  res.json(baiji)
 });
+
+// 
+router.get("/resverse/:name",(req,res)=>{
+    const resverse = [... req.params.name].reverse().join("")
+    res.send(resverse)
+})
 
 module.exports = router;
